b409d77a1031fdeac5cc80d7604c73481e3302b1
Jeremiah Oluwatosin Ogunniyi <jerexbambex@gmail.com>
2021-10-17 22:15:41 +0100

    user profile update completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
 
     public function image()
     {
-        if ($this->avatar == null) {
+        if ($this->avatar === null) {
             return 'https://res.cloudinary.com/biochar-initiative-of-nigeria/image/upload/v1634449963/Exquisite/133-1332476_crowd-of-users-transparent-user-icon-png-clipart.jpg';
         }
         return json_decode($this->avatar)->secure_url;
